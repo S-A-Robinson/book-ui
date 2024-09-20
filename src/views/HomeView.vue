748c0846d83dbd538efda1898627892a19a61cdc
Scott Robinson <scott.robinson4477@gmail.com>
2024-09-20 13:31:23 +0100

    added mouse position composable and used it on the home page to create a new cursor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import InputButton from '@/components/InputButton.vue'
+import { ref, computed } from 'vue';
+import InputButton from '@/components/InputButton.vue';
+import { useMouse } from '@/composables/mouse.js';
+
+const isHovered = ref(false);
+const { x, y } = useMouse();
+
+const clampedY = computed(() => {
+  return Math.max(56, y.value);
+})
 </script>
 
 <template>
-  <main class="flex flex-col-reverse md:flex-row justify-around items-center h-fit md:h-[calc(100vh-56px)] mb-16 md:mb-0">
+  <main class="flex flex-col-reverse md:flex-row justify-around items-center h-fit md:h-[calc(100vh-56px)] mb-16 md:mb-0 cursor-none">
     <div class="mx-8 flex flex-col items-center md:w-1/2 md:items-start">
       <h1 class="text-center text-6xl md:text-left md:text-8xl">Book Keeper</h1>
       <p class="my-16 text-lg">
@@ -12,12 +21,33 @@ import InputButton from '@/components/InputButton.vue'
         Whether you're a voracious reader or just getting started, Book Keeper is here to help you organize your book lists, celebrate your reading achievements, and discover your next great read.
         Start exploring your reading world with us today!
       </p>
-      <RouterLink to="/books">
-        <InputButton>Get Started</InputButton>
+      <RouterLink to="/books" @mouseover="isHovered = true" @mouseleave="isHovered = false">
+        <InputButton class="cursor-none">Get Started</InputButton>
       </RouterLink>
     </div>
     <div class="m-8 flex h-64 w-64 justify-center rounded-full bg-white md:min-w-96 md:min-h-96">
       <img src="/bookshelf.png" alt="a stack of books" />
     </div>
+    <div
+      v-if="y > 56"
+      :class="`absolute w-4 h-4 rounded-full transparent-white pointer-events-none transition-scale ${isHovered ? 'scale-up' : ''}`"
+      :style="{ left: `${x}px`, top: `${y}px`, transform: `translate(-50%, -50%)` }"
+    ></div>
   </main>
 </template>
+
+<style scoped>
+.transparent-white {
+  background-color: rgba(255, 255, 255, 0.4);
+}
+
+.scale-up {
+  scale: 3;
+  background-color: rgba(255, 255, 255, 0.7);
+}
+
+.transition-scale {
+  transition: scale 0.2s ease-out, background-color 0.2s ease-out;
+  transform-origin: 0 0;
+}
+</style>
